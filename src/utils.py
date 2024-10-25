c51ded9a49e49fa0b68b07cf2e1a9385880511ae
Vizy <93832503+ruthlessgoldie@users.noreply.github.com>
2024-10-25 18:19:16 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,9 @@
-# src/utils.py
-
 import re
-
 def load_and_preprocess(file_paths):
     all_text = ""
     for path in file_paths:
-        with open(path, 'r', encoding='utf-8') as f:
+        with open(path, 'r', encoding='iso-8859-9') as f:  # Alternatif kodlama
             text = f.read()
-            # Sadece istenen karakterleri bırak
             text = re.sub(r'[^A-Za-zÇĞİIÖŞÜçğıöşü0-9.,!?;:\-()\s]', ' ', text)
             text = re.sub(r'\s+', ' ', text)
             text = text.lower()  # Türkçe karakterler için lower() kullanıldı
